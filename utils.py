24dfb5446c953e154d3cc70ac357ada00b9bf70e
Husnu Baris Baydargil <83386897+HusnuBarisBaydargil@users.noreply.github.com>
2024-06-04 11:56:09 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,19 @@
 import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.distributed import DistributedSampler
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
 class ImagePaths(Dataset):
     def __init__(self, path, image_size=128):

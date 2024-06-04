e652ebc537063abd2a0a1285f588f8b06fd7d6f5
Husnu Baris Baydargil <83386897+HusnuBarisBaydargil@users.noreply.github.com>
2024-06-04 11:43:40 +0900

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,33 +1,53 @@
 import torch.nn as nn
-from helper_func import GELU, NonLocalBlock, LayerNorm
-import torch
+from utils import ResidualBlock, NonLocalBlock, GroupNorm, Swish, DownSampleBlock
 
 class Encoder(nn.Module):
-    def __init__(self, image_channels=1):
+    def __init__(self, args):
         super(Encoder, self).__init__()
-        self.channels = [32, 32, 64, 128, 128]
-        self.convs = nn.ModuleList([
-            nn.Conv2d(image_channels, self.channels[0], 3, 1, 1),
-            nn.Conv2d(self.channels[0], self.channels[1], 3, 2, 1),
-            nn.Conv2d(self.channels[1], self.channels[2], 3, 2, 1),
-            nn.Conv2d(self.channels[2], self.channels[3], 3, 2, 1),
-            nn.Conv2d(self.channels[3], self.channels[4], 3, 2, 1)
-        ])
-        self.nonlocal_blocks = nn.ModuleList([
-            NonLocalBlock(self.channels[i]) for i in range(1, 5)
-        ])
-        self.norms_gelus = nn.ModuleList([
-            LayerNorm(ch, 128 // (2**i), 128 // (2**i)) for i, ch in enumerate(self.channels)
-        ])
+        self.channels = [128, 128, 128, 256, 256, 512]
+        self.attn_resolutions = [16]
+        self.num_res_blocks = 2
+        self.resolution = 128
+
+        self.model = nn.Sequential(*self._build_layers(args))
+
+    def _initial_layers(self, args):
+        return [nn.Conv2d(args.image_channels, self.channels[0], 3, 1, 1)]
+
+    def _residual_and_downsample_layers(self):
+        layers = []
+        resolution = self.resolution
+
+        for i in range(len(self.channels) - 1):
+            in_channels = self.channels[i]
+            out_channels = self.channels[i + 1]
+            for _ in range(self.num_res_blocks):
+                layers.append(ResidualBlock(in_channels, out_channels))
+                in_channels = out_channels
+                if resolution in self.attn_resolutions:
+                    layers.append(NonLocalBlock(in_channels))
+            if i != len(self.channels) - 2:
+                layers.append(DownSampleBlock(out_channels))
+                resolution //= 2
+
+        return layers
+
+    def _final_layers(self, args):
+        in_channels = self.channels[-1]
+        return [
+            ResidualBlock(in_channels, in_channels),
+            NonLocalBlock(in_channels),
+            ResidualBlock(in_channels, in_channels),
+            GroupNorm(in_channels),
+            Swish(),
+            nn.Conv2d(in_channels, args.latent_dim, 3, 1, 1)
+        ]
+
+    def _build_layers(self, args):
+        layers = self._initial_layers(args)
+        layers.extend(self._residual_and_downsample_layers())
+        layers.extend(self._final_layers())
+        return layers
 
     def forward(self, x):
-        skips = []
-        all_blocks = [None] + list(self.nonlocal_blocks)
-        for idx, (conv, norm, nonlocal_block) in enumerate(zip(self.convs, self.norms_gelus, all_blocks)):
-            x = conv(x)
-            x = norm(x)
-            x = GELU()(x)
-            if nonlocal_block is not None:
-                x = nonlocal_block(x)
-            skips.append(x)
-        return x, skips
+        return self.model(x)

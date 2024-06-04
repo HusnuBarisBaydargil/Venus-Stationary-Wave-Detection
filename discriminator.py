4004ba2fff632f765f7d6356de7ed2d8604db48f
Husnu Baris Baydargil <83386897+HusnuBarisBaydargil@users.noreply.github.com>
2024-06-04 11:44:47 +0900

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -1,32 +1,24 @@
-import torch
 import torch.nn as nn
 
 class Discriminator(nn.Module):
-    def __init__(self, image_channels=1):
+    def __init__(self, args, num_filters_last=64, n_layers=3):
         super(Discriminator, self).__init__()
-        self.channels = [32, 64, 128, 128]
-        self.convs = nn.ModuleList([
-            nn.Conv2d(image_channels, self.channels[0], 3, 1, 1),
-            nn.Conv2d(self.channels[0], self.channels[1], 3, 1, 1),
-            nn.Conv2d(self.channels[1], self.channels[2], 3, 1, 1),
-            nn.Conv2d(self.channels[2], self.channels[3], 3, 1, 1)
-        ])
-        self.maxpools = nn.ModuleList([
-            nn.MaxPool2d(2) for _ in self.channels
-        ])
-        self.leaky_relus = nn.ModuleList([
-            nn.LeakyReLU(0.2) for _ in self.channels
-        ])
-        self.final_fc = nn.Linear(128 * 8 * 8, 1)
-        self.sigmoid = nn.Sigmoid()
+
+        layers = [nn.Conv2d(args.image_channels, num_filters_last, 4, 2, 1), nn.LeakyReLU(0.2)]
+        num_filters_mult = 1
+
+        for i in range(1, n_layers + 1):
+            num_filters_mult_last = num_filters_mult
+            num_filters_mult = min(2 ** i, 8)
+            layers += [
+                nn.Conv2d(num_filters_last * num_filters_mult_last, num_filters_last * num_filters_mult, 4,
+                          2 if i < n_layers else 1, 1, bias=False),
+                nn.BatchNorm2d(num_filters_last * num_filters_mult),
+                nn.LeakyReLU(0.2, True)
+            ]
+
+        layers.append(nn.Conv2d(num_filters_last * num_filters_mult, 1, 4, 1, 1))
+        self.model = nn.Sequential(*layers)
 
     def forward(self, x):
-        for conv, maxpool, leaky_relu in zip(self.convs, self.maxpools, self.leaky_relus):
-            x = conv(x)
-            x = maxpool(x)
-            x = leaky_relu(x)
-
-        x = x.view(x.size(0), -1)
-        x = self.final_fc(x)
-        x = self.sigmoid(x)
-        return x
+        return self.model(x)

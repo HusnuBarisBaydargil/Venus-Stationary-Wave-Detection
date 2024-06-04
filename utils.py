430e7245d37b0d36625e5296813d946c267cef0a
Husnu Baris Baydargil <83386897+HusnuBarisBaydargil@users.noreply.github.com>
2024-06-04 11:46:18 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,31 +5,126 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.distributed import DistributedSampler
 
 class ImagePaths(Dataset):
-    def __init__(self, path):
-        self.images = [os.path.join(path, file) for file in os.listdir(path)]
+    def __init__(self, path, image_size=128):
+        self.images = [os.path.join(path, file) for file in os.listdir(path) if file.endswith(('png', 'jpg', 'jpeg'))]
         self._length = len(self.images)
+        self.image_size = image_size
 
     def __len__(self):
         return self._length
 
     def preprocess_image(self, image_path):
-        image = Image.open(image_path)
+        image = Image.open(image_path).convert('L')  
+        image = image.resize((self.image_size, self.image_size), Image.BILINEAR)
         image = np.array(image).astype(np.float32)
-        image = (image / 127.5 - 1.0)
+        image = (image / 127.5) - 1.0
         image = image[np.newaxis, :, :]
-
+        
         return image
 
     def __getitem__(self, i):
         example = self.preprocess_image(self.images[i])
         return example
 
-def load_data(dataset_path, batch_size, is_distributed=False, rank=0, num_replicas=1):
-    train_data = ImagePaths(dataset_path)
+def load_data(dataset_path, batch_size, image_size=128, is_distributed=False, rank=0, num_replicas=1):
+    train_data = ImagePaths(dataset_path, image_size=image_size)
     if is_distributed:
         sampler = DistributedSampler(train_data, num_replicas=num_replicas, rank=rank, shuffle=True)
         train_loader = DataLoader(train_data, batch_size=batch_size, sampler=sampler, num_workers=10, pin_memory=True)
+        return train_loader, sampler
     else:
         train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=10, pin_memory=True)
-    
-    return train_loader, sampler if is_distributed else train_loader
+        return train_loader
+
+class GroupNorm(nn.Module):
+    def __init__(self, channels):
+        super(GroupNorm, self).__init__()
+        self.gn = nn.GroupNorm(num_groups=32, num_channels=channels, eps=1e-6, affine=True)
+
+    def forward(self, x):
+        return self.gn(x)
+
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.block = nn.Sequential(
+            GroupNorm(in_channels),
+            Swish(),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            GroupNorm(out_channels),
+            Swish(),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1)
+        )
+
+        if in_channels != out_channels:
+            self.channel_up = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
+
+    def forward(self, x):
+        if self.in_channels != self.out_channels:
+            return self.channel_up(x) + self.block(x)
+        else:
+            return x + self.block(x)
+
+
+class UpSampleBlock(nn.Module):
+    def __init__(self, channels):
+        super(UpSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 1, 1)
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=2.0)
+        return self.conv(x)
+
+
+class DownSampleBlock(nn.Module):
+    def __init__(self, channels):
+        super(DownSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 2, 0)
+
+    def forward(self, x):
+        pad = (0, 1, 0, 1)
+        x = F.pad(x, pad, mode="constant", value=0)
+        return self.conv(x)
+
+
+class NonLocalBlock(nn.Module):
+    def __init__(self, channels):
+        super(NonLocalBlock, self).__init__()
+        self.in_channels = channels
+
+        self.gn = GroupNorm(channels)
+        self.q = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.k = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.v = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.proj_out = nn.Conv2d(channels, channels, 1, 1, 0)
+
+    def forward(self, x):
+        h_ = self.gn(x)
+        q = self.q(h_)
+        k = self.k(h_)
+        v = self.v(h_)
+
+        b, c, h, w = q.shape
+
+        q = q.reshape(b, c, h*w)
+        q = q.permute(0, 2, 1)
+        k = k.reshape(b, c, h*w)
+        v = v.reshape(b, c, h*w)
+
+        attn = torch.bmm(q, k)
+        attn = attn * (int(c)**(-0.5))
+        attn = F.softmax(attn, dim=2)
+        attn = attn.permute(0, 2, 1)
+
+        A = torch.bmm(v, attn)
+        A = A.reshape(b, c, h, w)
+
+        return x + A
